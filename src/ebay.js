42cdc73fb6f77613423b2ddfef1eb32407903156
unknown <tofig.bakhshiyev@ogr.sakarya.edu.tr>
2020-06-08 00:35:14 +0400

    get ebay data

---
+++ src/ebay.js
@@ -0,0 +1,39 @@
+const puppeteer = require('puppeteer'); 
+
+let ebay = async(url) => {
+    try { 
+        const browser = await puppeteer.launch({ 
+            // ignore extensions on Windows 10
+            ignoreDefaultArgs: ['--disable-extensions'],
+            // headless true this means chrome page is not opened but you can do false
+            headless: true 
+        })
+        // create new page 
+        const page = await browser.newPage() 
+
+        await page.goto(url)  
+        
+        // evaluate the page
+        const products = await page.evaluate(() => {
+            // gets all items with span tag
+            const items = Array.from(document.querySelectorAll('div[class="s-item__info clearfix"]'))
+            return items.map(item => {
+                // textContent does not write null gives error because of that if else is needed
+                return {
+                    name: item.querySelector('h3').innerText,
+                    price: item.querySelector('.s-item__price').innerText,
+                    url: item.querySelector('a').href
+                };  
+            })
+        });  
+        // close browser
+        await browser.close()
+        // send all scraping data to client
+        return products
+    } catch (error) {
+        return ({'error': error})
+    }
+}
+ 
+
+module.exports = ebay
--- src/index.js
+++ src/index.js
@@ -2,17 +2,27 @@ const express = require('express')
 const app = express()
 
 const amazon = require('./amazon')
+const ebay = require('./ebay')
 
 const port = process.env.PORT || 4040
 
-app.get('/', async (req, res) => {
-    let url = 'https://www.amazon.com/s?k=pc'
-    
-    let products = await amazon(url)
+app.get('/', (req, res) => {
+    res.send('Add search term to url "/:search"')
+})
 
+app.get('/:search', async (req, res) => { 
+    // get parameter from url
+    let search = req.params.search
+    // making urls dynamic
+    let url_amazon = `https://www.amazon.com/s?k=${search}`
+    let url_ebay = `https://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw=${search}&_sacat=0`
+
+    let products_amazon = await amazon(url_amazon)
+    let products_ebay = await ebay(url_ebay)
     // send all scraping data to client
     res.send({ 
-        'amazon': products
+        'amazon': products_amazon,
+        'ebay': products_ebay
     })   
 })
 

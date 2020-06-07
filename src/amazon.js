912d3b168221a641e6c6b8e0e150566cdcf1b399
unknown <tofig.bakhshiyev@ogr.sakarya.edu.tr>
2020-06-07 23:32:47 +0400

    get data from amazon

---
+++ src/amazon.js
@@ -0,0 +1,49 @@
+const puppeteer = require('puppeteer'); 
+
+let amazon = async(url) => {
+    try { 
+        const browser = await puppeteer.launch({ 
+            // ignore extensions on Windows 10
+            ignoreDefaultArgs: ['--disable-extensions'],
+            // headless true this means chrome page is not opened but you can do false
+            headless: true 
+        })
+        // create new page 
+        const page = await browser.newPage() 
+
+        await page.goto(url)  
+        
+        // evaluate the page
+        const products = await page.evaluate(() => {
+            // gets all items with span tag
+            const items = Array.from(document.querySelectorAll('span[cel_widget_id="MAIN-SEARCH_RESULTS"]'));
+            return items.map(item => {
+                // textContent does not write null gives error because of that if else is needed 
+                // if item has price return 
+                if (item.querySelector(".a-price-whole")){
+                    return {
+                        name: item.querySelector('h2').innerText,
+                        price: item.querySelector('.a-link-normal.a-text-normal > span[class="a-price"] > span[class="a-offscreen"]').textContent.replace(/[Count,/]/g, m => (m === '' ? '' : '')),
+                        url: item.querySelector(".a-link-normal.a-text-normal").href
+                    }; 
+                } else { 
+                    // without price
+                    return {
+                        name: item.querySelector('h2').innerText,
+                        price: 'price is not given',
+                        url: item.querySelector(".a-link-normal.a-text-normal").href,
+                    }
+                }
+            })
+        }); 
+         
+        // close browser
+        await browser.close()
+        // send all scraping data to client
+        return products
+    } catch (error) {
+        return ({'error': error})
+    }
+}
+ 
+module.exports = amazon
--- src/index.js
+++ src/index.js
@@ -1,55 +1,19 @@
-const express = require('express')
-const puppeteer = require('puppeteer');
-
+const express = require('express') 
 const app = express()
+
+const amazon = require('./amazon')
 
 const port = process.env.PORT || 4040
 
 app.get('/', async (req, res) => {
-    (async () => {
-        try {
-            let url = 'https://www.imdb.com/title/tt2661044/'
-            let url1 = 'https://www.amazon.com/s?k=tea'
-            const browser = await puppeteer.launch({ 
-                ignoreDefaultArgs: ['--disable-extensions'],
-                headless: true 
-            })
-             
-            const page = await browser.newPage()
-            const page1 = await browser.newPage()
-            await page.goto(url)
-            await page1.goto(url1)
-            //await page.screenshot({path: 'example.png'});
-            let data = await page.evaluate(() => {
-                let creator = document.querySelector('div[class="credit_summary_item"] > a').innerText
-                let rating = document.querySelector('span[itemprop="ratingValue"]').innerText 
-                let count = document.querySelector('span[itemprop="ratingCount"]').innerText 
-                let summary = document.querySelector('div[class="summary_text"]').innerText
+    let url = 'https://www.amazon.com/s?k=pc'
+    
+    let products = await amazon(url)
 
-                return {
-                    creator,
-                    rating,
-                    count,
-                    summary
-                }
-            })
-
-            let data1 = await page1.evaluate(() => {
-                let item = document.querySelector('div[class="a-section a-spacing-none a-spacing-top-small"]').innerText
-
-                return { 
-                    item
-                }
-            })
-            res.send({
-                'imdb': data,
-                'amazon': data1
-            }) 
-            await browser.close()
-        } catch (error) {
-            res.send({'error': error})
-        }
-    })()
+    // send all scraping data to client
+    res.send({ 
+        'amazon': products
+    })   
 })
 
 
